13409c5df825f0c68a25570d6f4aec60c904bcf9
GoutamAgrawal <53086934+GoutamAgrawal@users.noreply.github.com>
2026-06-15 13:25:19 +0530

    Add operator implementations

--- csrc/ops/vision_ops.cpp
+++ csrc/ops/vision_ops.cpp
@@ -1,3 +1,22 @@
+// -----------------------------------------------------------------------------
+// Copyright (c) 2026 Centre for Development of Advanced Computing (C-DAC)
+//
+// This file is part of Torch_ParaS, a component of the ParaS Ecosystem
+//
+// This library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License (LGPL)
+// version 3 as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <https://www.gnu.org/licenses/>.
+// -----------------------------------------------------------------------------
+
+
 #include "core/kernels.h"
 #include <ATen/ATen.h>
 #include <torch/torch.h>
